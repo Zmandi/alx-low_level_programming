b03c287d5a2968db5c24de08f4aad36b8713df6a
Zmandi <asarekorleki@gmail.com>
2023-04-30 15:12:29 -0700

    0x04. C Tasks upd

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,38 +1,30 @@
 #include "main.h"
 /**
  * print_diagonal - draw a diagonal line
- * @n: Number of times the character \ should be printed
+ * @n: Number of times the character should be printed
  * Return: void
  */
 void print_diagonal(int n)
 {
+	int a, b;
+
 	if (n <= 0)
 
 	{
 		_putchar('\n');
 	}
-	else
-	{
-		int a, b;
 
-		for (a = 0; a < n; a++)
+	for (a = 0; a < n; a++)
+	{
+
+		for (b = 0; b < a; b++)
 		{
 
-			for (b = 0; b < n; b++)
-			{
-
-			if (a == b)
-
-			_putchar('\\');
-
-		else if (a < b)
-
 			_putchar(' ');
-			}
-
-		_putchar('\n');
 
 		}
+		_putchar('\\');
+		_putchar('\n');
 
 	}
 
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,26 +11,22 @@ int main(void)
 {
 	char a;
 
-	for (a = 0; a <= 100; a++)
+	for (a = 1; a <= 100; a++)
 	{
 		if (a % 3 == 0 && a % 5 != 0)
 		{
-			printf(" Fizz");
+			printf("Fizz ");
 
 		} else if (a % 5 == 0 && a % 3 != 0)
 		{
-			printf(" Buzz");
+			printf("Buzz ");
 
-		} else if (a % 3 == 0 && a % 5 == 0)
+		} else if (a % 3 ==0  && a % 5 == 0)
 		{
-			printf(" FizzBuzz");
-
-		} else if (a == 1)
-		{
-			printf("%d", a);
+			printf("FizzBuzz ");
 		} else
 		{
-			printf(" %d", a);
+			printf("%d ", a);
 		}
 
 	}
