0f7b80a15f021999f3ef72ab103ec202b2a87440
Zmandi <asarekorleki@gmail.com>
2023-05-16 13:30:04 -0700

    0x0B. C - malloc Task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,3 +8,39 @@
  */
 char *argstostr(int ac, char **av)
 {
+	int m, n, p = 0, s = 0;
+
+	char *arr;
+
+	if (ac == 0 || av == NULL)
+	return (NULL);
+
+	for (m = 0; m < ac; m++)
+	{
+		for (n = 0; av[m][n]; n++)
+		s++;
+	}
+
+	s += ac;
+
+	arr = malloc(sizeof(char) * s + 1);
+
+	if (arr == NULL)
+		return (NULL);
+	for (m = 0; m < ac ; m++)
+	{
+
+	for (n = 0; av[m][n]; n++)
+	{
+		arr[p] = av[m][n];
+		p++;
+	}
+	if (arr[p] == '\0')
+	{
+		arr[p++] = '\n';
+	}
+
+	}
+
+	return (arr);
+}
