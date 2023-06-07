e6fbdc00ba88ce65370818c63562ef18be8f3dc7
Zmandi <asarekorleki@gmail.com>
2023-06-06 22:50:03 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (x = 0; temp && x < idx; x++)
+	for (x = 0; temp && x < idx - 1; x++)
 	{
 		if (x == idx - 1)
 		{
@@ -39,5 +39,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		else
 			temp = temp->next;
 	}
+	free(new);
+
 	return (NULL);
 }
